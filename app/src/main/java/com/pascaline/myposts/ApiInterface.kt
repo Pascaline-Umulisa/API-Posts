9b7c0a224b6653e12d8af0cf90d5a75c7bc59236
Pascaline-Umulisa <kayiteteumulisa@gmail.com>
2022-08-20 10:36:27 +0300

    changed the tools bars and also fetched the post from another other activity

--- app/src/main/java/com/pascaline/myposts/ApiInterface.kt
+++ app/src/main/java/com/pascaline/myposts/ApiInterface.kt
@@ -2,8 +2,12 @@ package com.pascaline.myposts
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiInterface {
     @GET("/posts")
     fun getPosts():Call<List<Post>>
+
+    @GET("/posts/{postId}")
+    fun getPostById(@Path("postId")postId:Int):Call<Post>
 }
--- app/src/main/java/com/pascaline/myposts/CommentsActivity.kt
+++ app/src/main/java/com/pascaline/myposts/CommentsActivity.kt
@@ -2,7 +2,11 @@ package com.pascaline.myposts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.pascaline.myposts.databinding.ActivityCommentsBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class CommentsActivity : AppCompatActivity() {
     var postId=0
@@ -12,8 +16,32 @@ class CommentsActivity : AppCompatActivity() {
         binding=ActivityCommentsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         obtainPostId()
+        fetchPostId()
+        setUpToolbar()
     }
     fun obtainPostId(){
         postId=intent.extras?.getInt("POST_ID")?:0
+    }
+    fun fetchPostId(){
+        val apiClient=APIclient.buildApiClient(ApiInterface::class.java)
+        val request=apiClient.getPostById(postId)
+        request.enqueue(object : Callback<Post> {
+            override fun onResponse(call: Call<Post>, response: Response<Post>) {
+                if(response.isSuccessful){
+                    var post=response.body()
+                    binding.tvBodyC.text=post?.body
+                    binding.tvTitleC.text=post?.title
+                }
+            }
+
+            override fun onFailure(call: Call<Post>, t: Throwable) {
+                Toast.makeText(baseContext,t.message,Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+    fun setUpToolbar(){
+        setSupportActionBar(binding.tbComments)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
     }
 }
